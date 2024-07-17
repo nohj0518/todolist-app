55122af2f3e7d58d1f14875a6ef809cf03d4d036
nohj0518 <nohj0518@daum.net>
2024-07-18 00:54:31 +0900

    add feat create todo

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import ToDoItem from "./ToDoItem.tsx";
 
 function ToDoList(_props) {
-  const [todo, setText] = useState({});
+  const [title, setTitle] = useState("");
+  const [memo, setMemo] = useState("");
 
   const [todos, setTodos] = useState([
     { id: 1, title: "Sample Todo 1", memo: "This is a sample memo 1" },
@@ -18,18 +19,52 @@ function ToDoList(_props) {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
-  const createTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
+  const createTodo = (title, memo) => {
+    const newId = todos.length ? todos[todos.length - 1].id + 1 : 1;
+    setTodos([...todos, { id: newId, title, memo }]);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createTodo(title, memo);
+    setTitle("");
+    setMemo("");
+  };
+
+  const updateTodo = (id) => {};
+
+  const showTodoForm = () => {};
 
   return (
     <>
       <div>
-        <header>
-          <h2>오늘의 할일을 생성해보세요 !</h2>
-          <p>{formattedDate}</p>
-          <button onClick={() => this.onCreate(todo.id + 1)}>Create</button>
-        </header>
+        <form onSubmit={handleSubmit}>
+          <header>
+            <p>{formattedDate}</p>
+            <h2>오늘의 할일을 생성해보세요 !</h2>
+          </header>
+          <label htmlFor="title">할일 제목</label>
+          <input
+            name="title"
+            id="title"
+            value={title}
+            required
+            placeholder="할일 제목을 입력하세요"
+            size={60}
+            onChange={(e) => setTitle(e.target.value)}
+          ></input>
+          <label htmlFor="memo">할일 메모</label>
+          <input
+            name="memo"
+            id="memo"
+            value={memo}
+            required
+            placeholder="할일 메모를 입력하세요"
+            size={60}
+            onChange={(e) => setMemo(e.target.value)}
+          ></input>
+          <button type="submit">Create</button>
+        </form>
         <ToDoItem todos={todos} onDelete={deleteTodo} />
       </div>
     </>
