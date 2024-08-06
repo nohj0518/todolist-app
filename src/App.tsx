4cab916265c5ff710d6e624ffb619be8a361c663
nohj0518 <nohj0518@daum.net>
2024-08-06 23:34:35 +0900

    init setting tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import ToDoList from "./components/ToDoList.tsx";
+import "./global.css";
 
 function Main(_props) {
   const [message, setMessage] = useState("Hello, World!");
 
   return (
     <>
+      <head>
+        <link href="/dist/output.css" rel="stylesheet" />
+      </head>
       <link rel="stylesheet" href="https://unpkg.com/mvp.css"></link>
+
       <body>
         <header>
+          <h1 className="text-3xl font-bold underline red">Hello world!</h1>
           <div> 메인 헤더 welcome message : {message}</div>
         </header>
         <main>
